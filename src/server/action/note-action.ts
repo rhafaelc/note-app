3e3d90af1d537474353d35e781136e8c1901047c
Rhafael Chandra <109317539+rhafaelc@users.noreply.github.com>
2024-08-16 15:55:14 +0700

    fix: revalidate path

--- src/server/action/note-action.ts
+++ src/server/action/note-action.ts
@@ -4,6 +4,7 @@ import { actionClient } from "~/lib/safe-action";
 import { noteSchema } from "../form/note-schema";
 import { db } from "../db";
 import { notes } from "../db/schema";
+import { revalidatePath } from "next/cache";
 
 export const noteAction = actionClient
   .schema(noteSchema)
@@ -22,6 +23,7 @@ export const noteAction = actionClient
         if (!newNote[0]) {
           return { error: "Can't create a new note" };
         }
+        revalidatePath("/");
         return { success: `Created a note with title ${newNote[0].title}` };
       } catch (error) {
         return { error: "Can't create a new note" };
